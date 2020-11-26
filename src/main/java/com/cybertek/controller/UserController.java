4a96a05509add83f6f615723f2e0982a9db20124
Mehmet <mhmtsytrk@gmail.com>
2020-11-26 03:19:11 +0300

    User Created Part is Finished and Worked

--- src/main/java/com/cybertek/controller/UserController.java
+++ src/main/java/com/cybertek/controller/UserController.java
@@ -2,19 +2,16 @@ package com.cybertek.controller;
 
 import com.cybertek.dto.RoleDTO;
 import com.cybertek.dto.UserDTO;
-import com.cybertek.implementation.RoleServiceImpl;
 import com.cybertek.service.RoleService;
 import com.cybertek.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
 @Controller
 @RequestMapping("/user")
 public class UserController {
@@ -36,15 +33,8 @@ public class UserController {
 
     @PostMapping("/create")
     public String insertUser(UserDTO user,Model model){
-
         userService.save(user);
-
-        model.addAttribute("user",new UserDTO());
-        model.addAttribute("roles",roleService.findAll());
-        model.addAttribute("users",userService.findAll());
-
-        return "/user/create";
-
+        return "redirect:/user/create";
     }
 
     @GetMapping("/update/{username}")
@@ -59,14 +49,19 @@ public class UserController {
     }
 
     @PostMapping("/update/{username}")
-    public String updateUser(@PathVariable("username") String username,Model model){
-
-        model.addAttribute("user",new UserDTO());
-        model.addAttribute("roles",roleService.findAll());
-        model.addAttribute("users",userService.findAll());
-
-        return "/user/create";
+    public String updateUser(@PathVariable("username") String username,UserDTO user,Model model){
+        userService.update(user);
+        return "redirect:/user/create";
     }
+
+    @GetMapping("/delete/{username}")
+    public String deleteUser(@PathVariable("username") String username){
+        userService.deleteById(username);
+        return "redirect:/user/create";
+    }
+
+
+
 
 
 
